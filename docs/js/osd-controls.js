0fcdb91bb715c1564e4bc5dba15ec830bf129d5b
Tristan Roddis <tristanr@cogapp.com>
2017-12-08 12:25:11 +0000

    added pan

--- docs/js/osd-controls.js
+++ docs/js/osd-controls.js
@@ -5,6 +5,9 @@ $(function ()
     "use strict";
     $('#enhance').click(enhance);
     $('#pan-right').click(panRight);
+    $('#pan-left').click(panLeft);
+    $('#pan-up').click(panUp);
+    $('#pan-down').click(panDown);
   });
 
 function enhance() {
@@ -14,16 +17,38 @@ function enhance() {
     
     //viewer.viewport.getBounds()    
     //var point = new OpenSeadragon.Point(-0.5,-0.5)
-    viewer.viewport.zoomTo(viewer.viewport.getZoom()*2,null,true);
+    viewer.viewport.zoomTo(viewer.viewport.getZoom()*1.1,null,true);
 
 }
 
 function panRight() {
     "use strict";
-    console.log(viewer.viewport.getBounds())
+    pan(0.05,0);
+}
+
+function panLeft() {
+    "use strict";
+    pan(-0.05,0);
+}
+
+function panUp() {
+    "use strict";
+    pan(0,-0.05);
+}
+
+function panDown() {
+    "use strict";
+    pan(0,0.05);
+}
+
+function pan(x,y) {
+    "use strict";
+    console.log(viewer.viewport.getBounds());
+    var bounds = viewer.viewport.getBounds();
     
-    var point = new OpenSeadragon.Point(0.1,0)
-    viewer.viewport.panTo(point)    
+    var point = new OpenSeadragon.Point(x,y);
+    var newBounds = bounds.translate(point);
+    viewer.viewport.fitBounds(newBounds, true);    
 
 }
 
